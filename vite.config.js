6b0a3c4297042eb2ed174bac3401d9b966d149d0
RJErik <ritzerikjanos@gmail.com>
2025-10-27 22:30:16 +0100

    Update vite.config.js
    Made it so that it runs on admin instead  of user.

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/user", // Keep this to ensure assets are loaded from /user/ path
+  base: "/admin", // Keep this to ensure assets are loaded from /user/ path
   server: {
     port: 5174, // Secondary port (not directly accessed by users)
     proxy: {
